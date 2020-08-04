8034811c6e000fa880f058a97e662b2a8bef8ceb
Jens Hegner Stærmose <jensstaermose@hotmail.com>
2020-08-04 22:07:30 +0200

    In before format

--- i3/setupi3.sh
+++ i3/setupi3.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
-feh --bg-scale ./windows-xp-bliss.jpg
+pacman -S i3blocks
+
+feh --bg-scale ./backgrounds/windows-xp-bliss.jpg
+cp ../settings/.config/i3/config ~/.i3/config
+cp ../settings/etc/i3blocks.conf /etc/i3blocks.conf
+
+i3 reload
